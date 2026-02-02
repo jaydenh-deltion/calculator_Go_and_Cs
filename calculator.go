16b68626822927780546b7e1d368c46764926857
Jayden <97096023@st.deltion.nl>
2026-02-02 14:30:08 +0100

    uitleg

--- calculator.go
+++ calculator.go
@@ -3,11 +3,11 @@ package main
 import ("fmt")
 
 func getNumber(prompt string)(float64){
-
+// controleert of een getal is ingevoerd anders vraagt hij opnieuw om een getal
 	for {
 		var input float64
 		fmt.Print(prompt)
-		_, err := fmt.Scanf("%f", &input)
+		_, err := fmt.Scanf("%f", &input) 
 		if err != nil{
 			fmt.Print("Invalid input. Please enter a number: ")
 			fmt.Scanln()
@@ -23,7 +23,7 @@ func getOperator() string {
 		var oper string
 		fmt.Print("Enter operator: +, -, *, /:\n ")
 		_, err := fmt.Scanln(&oper)
-		if err != nil || (oper != "+" && oper != "-" && oper != "*" && oper != "/"){
+		if err != nil || (oper != "+" && oper != "-" && oper != "*" && oper != "/"){ // controleert of het 1 van deze operators is: +, -, *, /
 			fmt.Print("Invalid input. Please enter an operator: ")
 			continue
 		}
@@ -34,22 +34,25 @@ func getOperator() string {
 func main(){
 
 	for {
+// heet de gebruiker welkom 
+	fmt.Print("welcome to my calculator\n") 
 
-	fmt.Print("welcome to my calculator\n") // print function 
-
+// vraagt om het eerst getal
 	var num1 , num2 float64
 	num1 = getNumber("enter the first number: ")
 
+// vraagt om de operator
 	var operator string // variable declaration
 	operator = getOperator()
-	
+	// vraagt om het 2de getal
 	num2 = getNumber("enter the second number: ")
 
+	// delen door 0 is niet toegestaan
 		if operator == "/" && num2 == 0 {
 		fmt.Print("Error! Dividing by zero is not allowed.")
 		continue
 	}
-
+// doet de berekening
 		switch operator{
 	case "+":
 		fmt.Println(num1 + num2)
@@ -60,14 +63,16 @@ func main(){
 	case "/":
 		fmt.Println(num1 / num2)
 
+// geeft foutmelding
 	default:
 		fmt.Println("error! operator is not correct")
 	}
-		
+		// vraagt of je door wilt gaan 
 		fmt.Println("if you want to quit press N. Or if you want to continue press enter: " )
 		var quit string
 		fmt.Scanln(&quit)
 
+// sluit het programma als input N is 
 		if quit == "N"  {
 			fmt.Println("goodbye")
 			break
@@ -77,4 +82,4 @@ func main(){
 }}
 
 
-// go run calculator.go
+// go run calculator.go om te starten 
