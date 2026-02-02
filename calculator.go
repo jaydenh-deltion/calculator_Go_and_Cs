30acca6eaf1dffa2f5927ac2059d71f64482cb0d
Jayden <97096023@st.deltion.nl>
2026-02-02 10:53:43 +0100

    same as C++

--- calculator.go
+++ calculator.go
@@ -2,23 +2,55 @@ package main
 
 import ("fmt")
 
+func getNumber(prompt string)(float64){
+
+	for {
+		var input float64
+		fmt.Print(prompt)
+		_, err := fmt.Scanf("%f", &input)
+		if err != nil{
+			fmt.Print("Invalid input. Please enter a number: ")
+			fmt.Scanln()
+			continue
+		}
+		return input
+		
+	}
+}
+
+func getOperator() string {
+	for {
+		var oper string
+		fmt.Print("Enter operator: +, -, *, /:\n ")
+		_, err := fmt.Scanln(&oper)
+		if err != nil || (oper != "+" && oper != "-" && oper != "*" && oper != "/"){
+			fmt.Print("Invalid input. Please enter an operator: ")
+			continue
+		}
+		return oper
+		}
+	}
+
 func main(){
+
+	for {
+
 	fmt.Print("welcome to my calculator\n") // print function 
-	var operator string // variable declaration
-	fmt.Print("Enter operator: +, -, *, /:\n ")
-	fmt.Scan(&operator) // input for operator 
 
 	var num1 , num2 float64
-	fmt.Print("Enter first number: ")
-	fmt.Scan(&num1) // input num1
+	num1 = getNumber("enter the first number: ")
 
-	fmt.Print("Enter second number: ")
-	fmt.Scan(&num2) //input num2
+	var operator string // variable declaration
+	operator = getOperator()
+	
+	num2 = getNumber("enter the second number: ")
 
-	if operator == "/" && num2 == 0 {
+		if operator == "/" && num2 == 0 {
 		fmt.Print("Error! Dividing by zero is not allowed.")
+		continue
 	}
-	switch operator{
+
+		switch operator{
 	case "+":
 		fmt.Println(num1 + num2)
 	case "-":
@@ -27,9 +59,22 @@ func main(){
 		fmt.Println(num1 * num2)
 	case "/":
 		fmt.Println(num1 / num2)
+
 	default:
 		fmt.Println("error! operator is not correct")
 	}
-}
+		
+		fmt.Println("if you want to quit press N. Or if you want to continue press enter: " )
+		var quit string
+		fmt.Scanln(&quit)
+
+		if quit == "N"  {
+			fmt.Println("goodbye")
+			break
+		}
+	
+	
+}}
+
 
 // go run calculator.go
